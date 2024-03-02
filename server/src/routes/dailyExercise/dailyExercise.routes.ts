559e871c54bda38ce9918ed24b0d6ee7040de9df
hadeedtariq <computeranalog351@gmail.com>
2024-03-02 17:38:11 +0000

    /server: create get Today exercise api

--- server/src/routes/dailyExercise/dailyExercise.routes.ts
+++ server/src/routes/dailyExercise/dailyExercise.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { authChecker } from "../../middlewares/authChecker";
 import asyncHandler from "express-async-handler";
 import { DailyExercise } from "./dailyExercise.model";
-
 const router = Router();
 
 router.use(authChecker);
@@ -23,6 +22,22 @@ router.post(
     });
     if (userTodayExercise) {
       res.status(201).json({ message: "You exercise today successfully" });
+    }
+  })
+);
+
+router.get(
+  "/today",
+  asyncHandler(async (req, res, next) => {
+    const exercise = await DailyExercise.findOne({
+      user: req.body.user._id,
+    }).sort({ createdAt: -1 });
+
+    if (exercise) {
+      res.status(200).json(exercise);
+      return;
+    } else {
+      next({});
     }
   })
 );
