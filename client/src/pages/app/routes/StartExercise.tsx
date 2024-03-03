7de5f7a4b58d7afd0b0762c931ef62be01145549
hadeedtariq <computeranalog351@gmail.com>
2024-03-03 18:06:22 +0000

    created exercise scheduling and workout handling functionality

--- client/src/pages/app/routes/StartExercise.tsx
+++ client/src/pages/app/routes/StartExercise.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import { GrStatusGood } from "react-icons/gr";
+
 import { dayFormatter } from "../utils/dayFormatter";
-import { exercisesApi } from "../../../utils/axios";
+import { dailyExerciseApi, exercisesApi } from "../../../utils/axios";
 import { Exercise } from "../types/appTypes";
 import WorkoutHandler from "../_components/WorkoutHandler";
 import ExerciseNotCreatedYet from "../_components/ExerciseNotCreatedYet";
@@ -8,6 +10,21 @@ import ExerciseNotCreatedYet from "../_components/ExerciseNotCreatedYet";
 const StartExercise = () => {
   const date = new Date().getDay();
   const day = dayFormatter(date);
+
+  const { data, error } = useQuery({
+    queryKey: ["dailyExercise"],
+    queryFn: async () => {
+      const { data }: { data: Exercise } = await dailyExerciseApi.get("/today");
+      if (data) {
+        const todayDate = new Date();
+        const exerciseDate = new Date(data?.createdAt);
+        const userExerciseTodayOrNot =
+          exerciseDate.toLocaleDateString() === todayDate.toLocaleDateString();
+        return userExerciseTodayOrNot;
+      }
+    },
+  });
+
   const { data: exercise, isLoading } = useQuery({
     queryKey: [`TodayExercise`],
     queryFn: async () => {
@@ -22,7 +39,14 @@ const StartExercise = () => {
   if (isLoading) return <p>Loading...</p>;
   return (
     <>
-      {exercise ? (
+      {!error && data ? (
+        <div className="flex items-center justify-center w-full min-h-[100vh]">
+          <h1 className="text-3xl font-bold font-kode-mono flex items-center gap-2">
+            You have Completed Today exercise{" "}
+            <GrStatusGood size={28} color="green" />
+          </h1>
+        </div>
+      ) : exercise ? (
         <WorkoutHandler exercise={exercise} />
       ) : (
         <ExerciseNotCreatedYet day={day} />
